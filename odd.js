f0bb300cafa0b966858cf1c74a431c1da922d698
vagrant <aaron.bolarinho@gmail.com>
2018-09-11 23:52:31 +0000

    odd.js file work

---
+++ odd.js
@@ -0,0 +1,23 @@
+/*function isOdd(x) {
+  if (x % 2 !== 0)
+    return "true"
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+/*
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
+*/
+
+function isOdd(x) {
+  return x % 2 !== 0;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
